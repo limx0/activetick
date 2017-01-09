794657521601474edd6539f2c640f32a3444c56d
limx0 <bradley.mcelroy@live.com>
2017-01-09 22:04:12 +1100

    Add simple tests

--- activetick/tests/test_activetick.py
+++ activetick/tests/test_activetick.py
@@ -1,50 +1,33 @@
 
 import unittest
-import os
 import datetime
-import subprocess
-from multiprocessing import Process
-from activetick.io import get_option_chain, request_quote_ticks, request_bar_data, request_trade_ticks
+from activetick.core import get_option_chain, request_quote_ticks, request_bar_data, request_trade_ticks
+#from activetick.http_server import run_http_server
 
 SYMBOL = 'MSFT'
 
 
-def get_dates():
-    today = datetime.datetime.now()
-    last_friday = (today - datetime.timedelta(days=(today.weekday())+3))
-    return last_friday.replace(hour=0, minute=0), last_friday.replace(hour=23, minute=59)
+class TestActivetick(unittest.TestCase):
 
-
-def run_http_server(*args):
-    subprocess.Popen(list(args), stdout=subprocess.PIPE)
-
-
-class TestBase(unittest.TestCase):
-    BINARY = r'C:\Program Files\ActiveTick LLC\ActiveTick Feed HTTP Server\ActiveTickFeedHttpServer.exe'
-    SERVER = 'activetick1.activetick.com'
-    HOST = '127.0.0.1'
-    PORT = '5000'
-    API_KEY = os.environ['ACTIVETICK_API_KEY']
-    USERNAME = os.environ['ACTIVETICK_USERNAME']
-    PASSWORD = os.environ['ACTIVETICK_PASSWORD']
-
-    def setUp(self):
-        args = (self.BINARY, self.HOST, self.SERVER, self.PORT, self.API_KEY, self.USERNAME, self.PASSWORD)
-        self.p = Process(target=run_http_server, args=args)
-        self.p.start()
-
-    def tearDown(self):
-        self.p.terminate()
-
-
-class TestActivetick(TestBase):
-
-    date_from, date_to = get_dates()
+    date_from = datetime.datetime(2016, 1, 5, 11, 0)
+    date_to_short = datetime.datetime(2016, 1, 5, 11, 1)
+    date_to_long = datetime.datetime(2016, 1, 6, 11, 0)
+    date_to_vlong = datetime.datetime(2016, 1, 10, 11, 0)
 
     def test_option_chain(self):
         data = get_option_chain(SYMBOL, debug=True)
-        print(data)
-        self.assertTrue(data.shape)
+        self.assertAlmostEquals(data.shape[0], 800, -2)
 
     def test_quote_ticks(self):
-        self.assertTrue(request_quote_ticks(SYMBOL, self.date_from, self.date_to, debug=True))
+        data = request_quote_ticks(SYMBOL, self.date_from, self.date_to_short, debug=True)
+        self.assertEqual(data.shape[0], 1117)
+
+    def test_trade_ticks(self):
+        data = request_trade_ticks(SYMBOL, self.date_from, self.date_to_short, debug=True)
+        self.assertEqual(data.shape[0], 318)
+
+    def test_bar_data(self):
+        data = request_bar_data(SYMBOL, self.date_from, self.date_to_vlong, debug=True)
+        self.assertEqual(data.shape[0], 4)
+
+
